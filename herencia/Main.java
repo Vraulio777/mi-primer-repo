6cf6cf639f6d75d146b962642f1b56d09921b4d6
Belem Alcántara Díaz <belytrabajowesos09@gmail.com>
2026-05-27 22:57:28 -0600

    feat(delfin): complete metodos en delfin y los agregue en el main

--- herencia/Main.java
+++ herencia/Main.java
@@ -1,15 +1,23 @@
 package meerkat.ops.eliminar;
 
+import meerkat.ops.eliminar.implementacion.Delfin;
 import meerkat.ops.eliminar.implementacion.Perro;
 
 public class Main {
 
     static void main(String[] args) {
         Perro perro = new Perro();
+        Delfin delfin = new Delfin();
+
         System.out.println("=============>");
         perro.alimentarse();
         perro.desplazarse();
         perro.dormir(30);
+
+        System.out.println("==============>");
+        delfin.alimentarse();
+        delfin.desplazarse();
+        delfin.dormir(15);
     }
 
 }
